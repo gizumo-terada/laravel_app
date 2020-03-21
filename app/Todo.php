684f38b8a6b8ef7f3ba0eb7017d37a08126e308d
KunioTerada <denair_coyotes@icloud.com>
2020-03-21 18:32:05 +0900

    ソフトデリートに変更

--- app/Todo.php
+++ app/Todo.php
@@ -3,11 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['title','user_id'];
-
+    protected $dates = ['deleted_at'];
+    
     public function getByUserId($id)
     {
         return $this->where('user_id', $id)->get();
