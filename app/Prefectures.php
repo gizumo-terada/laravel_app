3b97dbc501d4aa4e10d09009593b193263f561f8
KunioTerada <denair_coyotes@icloud.com>
2020-04-15 16:26:24 +0900

    県庁所在地の初期データを用意して投入

---
+++ app/Prefectures.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prefectures extends Model
+{
+    protected $fillable = [
+        'pref',
+        'city',
+    ];
+    //
+}
